1ee8014f3056528698c509cbbf37d7877fa521ce
Olabanjo <olabanjoolaleye@gmail.com>
2025-10-31 13:04:45 +0100

    refactor color mode components; comment out unused code and update provider to accept children

--- src/hooks/useTransactions.ts
+++ src/hooks/useTransactions.ts
@@ -6,7 +6,8 @@ export const useTransactions = () => {
   return useQuery({
     queryKey: ["transactions"],
     queryFn: api.getTransactions,
-    placeholderData: mockTransactions,
+    // placeholderData: mockTransactions,
+    placeholderData: [],
     staleTime: 5 * 60 * 1000, // 5 minutes
   });
 };
